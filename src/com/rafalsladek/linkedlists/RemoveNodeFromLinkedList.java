f8641f26fc7baac08f818fda21b2a279ce4436bd
Rafal Sladek <rafal.sladek@gmail.com>
2016-11-18 19:43:56 +0100

    removed unused class RemoveNodeFromLinkedList

--- src/com/rafalsladek/linkedlists/RemoveNodeFromLinkedList.java
+++
@@ -1,8 +0,0 @@
-package com.rafalsladek.linkedlists;
-
-public class RemoveNodeFromLinkedList {
-    public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.add(new Node(2));
-    }
-}
--- test/com/rafalsladek/linkedlists/RemoveNodeFromLinkedListTest.java
+++
@@ -1,7 +0,0 @@
-package com.rafalsladek.linkedlists;
-
-import static org.junit.Assert.*;
-
-public class RemoveNodeFromLinkedListTest {
-
-}
